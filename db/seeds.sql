169ac826d0767ad0e5327de73dbf2d5a3b788d73
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-09-10 23:10:17 -0400

    set up DB

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+use burgers_db;
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Sloppy Joe", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey", true);
