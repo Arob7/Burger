f761eae77bbd2ee21daea1cd5e05f3e1416e1daf
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-09-11 23:23:57 -0400

    added connection, .gitignore, installed dotenv

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: process.env.mySqlPW,
+  database: "burgers_db",
+});
