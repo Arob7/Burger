13a64bd7c572ebea96c473a95d179dabbe603b6c
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-09-16 00:26:58 -0400

    API PUT METHOD WORKS

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,4 @@
+// BUTTON TO CREATE NEW BURGER
 $(function () {
   $(".create-form").on("submit", function (event) {
     event.preventDefault();
@@ -5,13 +6,27 @@ $(function () {
       burger_name: $("#newBurger").val().trim(),
       devoured: 0,
     };
-    // console.log(newBurger);
+    console.log(newBurger);
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger,
     }).then(function () {
       console.log("created new burger");
       // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+  $(".eat-burg").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1,
+    };
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState,
+    }).then(function () {
+      console.log("Ate the Burger!");
       location.reload();
     });
   });
