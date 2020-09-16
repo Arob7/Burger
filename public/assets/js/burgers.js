5cc66e14c19542e34cdf4562447253d89d3429fb
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-09-15 23:45:59 -0400

    Post function works

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -5,15 +5,14 @@ $(function () {
       burger_name: $("#newBurger").val().trim(),
       devoured: 0,
     };
-    console.log(newBurger);
+    // console.log(newBurger);
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
-});
-
-$.ajax("/api/burgers", {
-  type: "POST",
-  data: newBurger,
-}).then(function () {
-  console.log("created new burger");
-  // Reload the page to get the updated list
-  location.redirect();
 });
