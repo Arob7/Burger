0ce4e5e90e21cf741f0a195011015d5f739e06fb
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-09-16 18:16:19 -0400

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.mySqlPW,
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.mySqlPW,
+    database: "burgers_db",
+  });
+}
 
 // MAKE THE CONNECTION
 connection.connect(function (err) {
--- config/orm.js
+++ config/orm.js
@@ -30,7 +30,7 @@ function objToSql(ob) {
 }
 // OBJECT FOR ALL SQL STATEMENT FUNCTIONS
 var orm = {
-  all: function (tableInput, cb) {
+  selectAll: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function (err, result) {
       if (err) {
@@ -39,7 +39,7 @@ var orm = {
       cb(result);
     });
   },
-  create: function (table, cols, vals, cb) {
+  insertOne: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -60,7 +60,7 @@ var orm = {
     });
   },
 
-  update: function (table, objColVals, condition, cb) {
+  updateOne: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
